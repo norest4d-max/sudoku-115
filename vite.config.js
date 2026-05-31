a48cbbe595e5a92dcd7137fd41ccc739d5adbeca
norest4d-max <no_rest4d@icloud.com>
2026-05-31 16:12:50 -0700

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/sudoku-115/',
   plugins: [react()],
   build: {
     outDir: 'dist'
